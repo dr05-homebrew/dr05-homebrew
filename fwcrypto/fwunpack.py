fb9f386bbc97773db708eac616dc810a6fefa048
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2016-08-25 02:40:47 +0200

    fwunpack: fixed offset and checksum at the end

---
+++ fwcrypto/fwunpack.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+from __future__ import division
+import os
+import sys
+import numpy as np
+import cv2
+
+from fwutils import * # invert_permutation
+
+permfile, firmwarefile = sys.argv[1:]
+
+perm = np.memmap(permfile, shape=(256), dtype=np.uint8, mode='r')
+firmware = np.memmap(firmwarefile, dtype=np.uint8, mode='r')
+decoded = np.memmap(firmwarefile + ".bin", shape=(len(firmware) - 32 - 4), mode='w+', dtype=np.uint8)
+
+iperm = invert_permutation(perm)
+
+decoded[:] = iperm[firmware[32:-4]]
+
