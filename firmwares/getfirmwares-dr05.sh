ce086b1652d517c0bb8110315687069e24fc5ea6
22459 <pie_>
2016-08-25 19:29:30 +0200

    Added downloader scripts for firmware update files

---
+++ firmwares/getfirmwares-dr05.sh
@@ -0,0 +1,8 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
+wget 'http://tascam.com/content/downloads/products/558/dr05-29.zip'
+wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v111.zip'
+wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v112.zip'
+wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v200.zip'
+wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v210.zip'
+wget 'http://tascam.com/content/downloads/products/558/dr-05_2.11.zip'
+
---
+++ firmwares/getfirmwares-dr40.sh
@@ -0,0 +1,11 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+wget 'http://tascam.com/content/downloads/products/706/dr40_056.zip'
+wget 'http://tascam.com/content/downloads/products/706/dr40_062.102'
+wget 'http://tascam.com/content/downloads/products/706/dr40_066.110.zip'
+wget 'http://tascam.com/content/downloads/products/706/dr40_v120.zip'
+wget 'http://tascam.com/content/downloads/products/706/dr-40_v130.zip'
+wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v200.zip'
+wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v210.zip'
+wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v211.zip'
+wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v212.zip'
+
