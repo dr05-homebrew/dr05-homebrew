e8a6a2eb5e26c51c027b9fad1ac602660c80ec6d
Christoph Rackwitz <christoph.rackwitz@gmail.com>
2016-08-25 02:53:45 +0200

    fwpack: roundtripping well, only diff is date

--- fwcrypto/fwpack.py
+++ fwcrypto/fwpack.py
@@ -2,31 +2,39 @@
 import os
 import sys
 import struct
+import datetime
 import numpy as np
 from fwutils import *
 
 permfile, flashfile, outfile = sys.argv[1:]
 
+pagesize = 512
+
 perm = np.memmap(permfile, dtype=np.uint8, mode='r')
 flash = np.memmap(flashfile, dtype=np.uint8, mode='r')
 
-length = len(flash)
 checksum = flash.sum()
 
-version, version2 = 211,44
-year, day, month = 2016, 18, 5
+length = len(flash)
+paddedlength = roundup(length + 4, pagesize)
+outlength = 32 + paddedlength
 
-outlen = 32 + len(flash) + 4
+version = 1337
+revision = 42
+now = datetime.datetime.now()
+year, month, day = now.year, now.month, now.day
 
-out = np.memmap(outfile, shape=outlen, dtype=np.uint8, mode='w+')
+out = np.memmap(outfile, shape=outlength, dtype=np.uint8, mode='w+')
 
 out[0:32] = np.fromstring(struct.pack(">8sHHHHII8s",
 	"DR-05\0\0\0",
-	version, version2,
+	version, revision,
 	year, day * 100 + month,
 	checksum,
-	length + 32 + 4,
+	outlength,
 	'\0' * 8
 ), dtype=np.uint8)
-out[32:][:len(flash)] = perm[flash]
+
+out[32:32+len(flash)] = perm[flash]
+out[32+len(flash):-4] = 0xff
 out[-4:] = perm[np.fromstring(struct.pack(">I", checksum), dtype=np.uint8)]
