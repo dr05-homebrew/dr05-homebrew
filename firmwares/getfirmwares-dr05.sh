6ad12ef1d42bf1909670251122be8e02a6121f6d
Martin Heistermann <git@mheistermann.de>
2016-08-29 14:24:34 +0200

    getfirmwares-*.sh: Add shebang, wget -c to avoid .1 files.

--- firmwares/getfirmwares-dr05.sh
+++ firmwares/getfirmwares-dr05.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #http://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
-wget 'http://tascam.com/content/downloads/products/558/dr05-29.zip'
-wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v111.zip'
-wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v112.zip'
-wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v200.zip'
-wget 'http://tascam.com/content/downloads/products/558/dr-05_fw_v210.zip'
-wget 'http://tascam.com/content/downloads/products/558/dr-05_2.11.zip'
+wget -c 'http://tascam.com/content/downloads/products/558/dr05-29.zip'
+wget -c 'http://tascam.com/content/downloads/products/558/dr-05_fw_v111.zip'
+wget -c 'http://tascam.com/content/downloads/products/558/dr-05_fw_v112.zip'
+wget -c 'http://tascam.com/content/downloads/products/558/dr-05_fw_v200.zip'
+wget -c 'http://tascam.com/content/downloads/products/558/dr-05_fw_v210.zip'
+wget -c 'http://tascam.com/content/downloads/products/558/dr-05_2.11.zip'
 
--- firmwares/getfirmwares-dr40.sh
+++ firmwares/getfirmwares-dr40.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-wget 'http://tascam.com/content/downloads/products/706/dr40_056.zip'
-wget 'http://tascam.com/content/downloads/products/706/dr40_062.102'
-wget 'http://tascam.com/content/downloads/products/706/dr40_066.110.zip'
-wget 'http://tascam.com/content/downloads/products/706/dr40_v120.zip'
-wget 'http://tascam.com/content/downloads/products/706/dr-40_v130.zip'
-wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v200.zip'
-wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v210.zip'
-wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v211.zip'
-wget 'http://tascam.com/content/downloads/products/706/dr-40_fw_v212.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr40_056.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr40_062.102'
+wget -c 'http://tascam.com/content/downloads/products/706/dr40_066.110.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr40_v120.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr-40_v130.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr-40_fw_v200.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr-40_fw_v210.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr-40_fw_v211.zip'
+wget -c 'http://tascam.com/content/downloads/products/706/dr-40_fw_v212.zip'
 
